6900cd6632acc80a1efb0cbec4f4f496f3bce792
Mário Santos <mario.santos.silva@gmail.com>
2021-07-28 17:16:14 -0300

    Inicio do projeto RHVirtual

---
+++ src/main/java/com/ams/rhvirtual/RhVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.ams.rhvirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ams/rhvirtual/RhVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.rhvirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
